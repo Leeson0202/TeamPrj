3b5cbd23d800a11570e59869c47edf58fd8f89dd
Leeson0202 <asedrfa@163.com>
2020-11-12 14:43:39 +0800

    11-12成品

---
+++ __init__.py
@@ -0,0 +1,195 @@
+from requests import *
+import json
+from spider1 import *
+from spider2 import *
+import spider2
+import time
+
+header = {"user-agent": 'Mozilla/5.0'}  # 创建一个字段 浏览器5.0
+url_head_1 = "https://www.koolearn.com"
+url_head_2 = "http://www.iciba.com/word?w="
+
+
+class vocabulary:
+    # word_spell Mean_tag__2vGcf clearfix sentences分别为: str str 列表 词典
+    def __init__(self, word_spell=None, Mean_tag__2vGcf=None, clearfix=None, sentences=None):
+        self.word_spell = word_spell  # 单词的拼写
+        self.Mean_tag__2vGcf = Mean_tag__2vGcf  # 四六级、高中
+        self.clearfix = clearfix  # 词性和翻译
+        self.sentence = sentences  # 单词的例句
+
+
+def get_HTTP_response(url=None, params=None):
+    """
+    获取response并返回
+    """
+    if url:
+        try:
+            r = get(url, headers=header, params=params,
+                    timeout=10)  # 伪装浏览器进行爬取
+            r.raise_for_status()  # 自动检测爬虫状态=200
+        except:
+            return None
+        r.encoding = 'utf-8'  # 转换格式
+        return r  # 返回response
+    else:
+        return None
+
+
+def MyBeautifulSoup(soup=None, rex=None):
+    '''
+    提取关键词 并返回给相应的函数
+    '''
+
+    if rex == 1:
+        word_list = soup.find_all('a')
+        word_list = [x.string for x in word_list]
+        my_word_list =list()
+        for i in word_list:
+            x = "".join(x for x in i if ord(x) < 256)
+            my_word_list.append(x)
+        return my_word_list
+    else:
+        word_spell = Mean_tag__2vGcf = ''  # 初始化
+        clearfixs = list()
+        sentences = dict()
+        word_box = None
+        try:
+            word_box = soup.find_all(
+                name='div', attrs={"class": "FoldBox_fold__1GZ_2"})  # 找到 单词简介box
+            if word_box:
+                word_box = word_box[0]
+                word_spell = word_box.find_all(
+                    name='h1', attrs={"class": "Mean_word__3SsvB"})  # 找到单词拼写
+                if (not word_spell):
+                    word_spell = word_box.find_all(
+                        name='h2', attrs={"class": "Mean_sentence__2NXAD"})  # 找到单词拼写
+
+                Mean_tag__2vGcf = word_box.find_all(
+                    name='p', attrs={"class": "Mean_tag__2vGcf"})  # 找到 标签 四六级
+                if Mean_tag__2vGcf:  # 如果 有标签
+                    Mean_tag__2vGcf = Mean_tag__2vGcf[0].text
+                else:
+                    Mean_tag__2vGcf = ''
+
+                Mean_part = word_box.find_all(
+                    name='ul', attrs={"class": "Mean_part__1RA2V"})  # 找到 词性 及 翻译
+                if Mean_part:
+                    clearfixs = [x.text for x in Mean_part[0].contents]
+                else:
+                    Mean_part = word_box.find_all(
+                        name='h3', attrs={"class": "Mean_title__2BwLF"})  # 找到 词性 及 翻译
+                    if Mean_part:
+                        clearfixs = Mean_part[0].nextSibling.contents[0]
+
+                sentences_box = soup.find_all(
+                    name='div', attrs={"class": "SceneSentence_scene__1Dnz6"})  # 找到 例句box
+                if sentences_box:  # 有例句则 提取出字典
+                    sentences_e = sentences_box[0].find_all(
+                        name='p', attrs={"class": "NormalSentence_en__3Ey8P"})  # 例句
+                    sentences_c = sentences_box[0].find_all(
+                        name='p', attrs={"class": "NormalSentence_cn__27VpO"})  # 例句翻译
+                    if len(sentences_c) > 4:   # 提取并合成字典
+                        sentences_e = [e.text for e in sentences_e[0:5]]
+                        sentences_c = [c.text for c in sentences_c[0:5]]
+                        sentences = dict(zip(sentences_e,sentences_c))
+                    else:
+                        sentences_e = [e.text for e in sentences_e]
+                        sentences_c = [c.text for c in sentences_c]
+                        sentences = dict(zip(sentences_e, sentences_c))
+                else:
+                    sentences = dict()
+            return word_spell[0].text, Mean_tag__2vGcf, clearfixs, sentences
+        except:
+            return word_spell[0].text, Mean_tag__2vGcf, clearfixs, sentences
+
+
+def word_sort(path):
+    """
+    词汇爬取后 进行去重 排序
+    """
+    lines = list()  # 初始化
+    try:
+        with open(path, 'r', encoding='utf-8') as ff:
+            lines = ff.readlines()
+            lines = list(set(x.strip() for x in lines))
+            pass
+    except:
+        print('文件加载失败')
+        return
+    lines.sort()
+    try:
+        with open(path, "w", encoding='utf-8') as f:
+            for i in lines:
+                f.write(i + '\n')
+        print("保存成功")
+    except:
+        print("保存失败")
+        return
+
+
+def to_progress(id=0):
+    """
+    1. 根据id 进入不同的函数
+
+    """
+    if id == 1:  # ----爬取字母列表
+        # url = url_head_1 + 'dict/zimu_' + chr(ord('a')) + '_1.html'  # 单个字母测试
+        for a in range(ord('a'), ord('z') + 1):
+            url = url_head_1 + '/dict/zimu_' + chr(a) + '_1.html'  # 生成一个字母的首url链接
+            # print(url)
+            spider_1(url)  # 进入spider_1
+            # ---------------------------------去重排序算法
+            path = './datas/txt/' + chr(a) + '.txt'
+            word_sort(path=path)
+        exit()
+
+    elif (id == 2):  # ----搜索词汇
+        pass
+
+    elif (id == 3):  # ----重新搜索词汇详情
+        for i in range(ord('s'), ord('z') + 1):
+            path1 = './datas/txt/' + chr(i) + '.txt'
+            path2 = './datas/json/' + chr(i) + '.json'
+            start = time.time()
+            spider2.spider_2(path1, path2, letter=chr(i), key_word='',start = start)
+
+    elif (id == 4):  # ----继续搜索词汇详情
+        a = 'a'
+        key_word = 'A bed of roses'
+        try:
+            with open("./datas/daily/record.txt", 'r') as ff:  # 读取记录
+                lines = ff.readlines()
+            a, key_word = lines[0].strip(), lines[1].strip()  # 获取上次字母和单词位置
+        except:
+            to_progress(id=3)
+            exit()
+        for i in range(ord(a), ord('z') + 1):
+            path1 = './datas/txt/' + chr(i) + '.txt'
+            path2 = './datas/json/' + chr(i) + '.json'
+            start = time.time()
+            spider2.spider_2(path1, path2, letter=chr(i), key_word=key_word, start = start)
+        exit()
+    else:
+        exit()
+
+
+def main():
+    """
+    主函数
+    """
+    while True:
+        id = eval(input("""
+1. 收集词汇表       2. 搜索词汇
+3. 重新收集词汇详情  4.继续搜索词汇详情
+5. 退出
+请输入序号---------》"""))
+
+        if id < 5 and id > 0:
+            to_progress(id)  # 进入分进程函数
+        else:
+            return
+
+
+if __name__ == '__main__':
+    main()
--- spider1.py
+++ spider1.py
@@ -1,4 +1,5 @@
-from spider import *
+from __init__ import *
+from bs4 import BeautifulSoup
 
 
 url_head_1 = "https://www.koolearn.com"
@@ -10,12 +11,13 @@ def spider_1(url=None):
     url_list = list()    # 初始化 url列表
     url_list.append(url)   # 进表
     ok = False          # 当前进度是否保存
+    f_txt = open('./datas/txt/'+url[-8:-7]+'.txt', "w", encoding='utf-8')
 
     try:
         while url_list:  # 判断列表是否还有 url
             ok = False
             word_list = list()   # 单词列表清空
-            print(url_list[0])    # 输出当前 url
+            print('\r',url_list[0],end='')    # 输出当前 url
             response = get_HTTP_response(url=url_list[0])   # 得到响应
             del url_list[0]  # 删除列表
 
@@ -26,10 +28,11 @@ def spider_1(url=None):
                                 "class": "word-box"})[0]  # 找到 单词 box
             word_list.extend(MyBeautifulSoup(box, rex=1)) # 获取单词列表
 
-            with open('./datas/txt/'+url[-8:-7]+'.txt', "a", encoding='utf-8') as f:   # 保存单词
-                for i in word_list[::-1]:
-                    f.write(i+'\n')
-                ok = True
+            f_txt.seek(0,2)
+            for i in word_list[::-1]:
+                f_txt.write(i+'\n')
+            f_txt.flush()
+            ok = True
 
             next = soup.find_all(
                 name='a', attrs={"class": "next"})          # 查找 下一页
@@ -44,10 +47,9 @@ def spider_1(url=None):
     finally:
         if ok:
             try:
-                with open('./datas/txt/'+url[-8:-7]+'.txt', "a", encoding='utf-8') as f:
-                    for i in word_list[::-1]:
-                        f.write(i+'\n')
-                print("保存成功")
+                f_txt.seek(0, 2)
+                for i in word_list[::-1]:
+                    f_txt.write(i + '\n')
+                f_txt.flush()
             except:
                 print("保存失败")
-   
